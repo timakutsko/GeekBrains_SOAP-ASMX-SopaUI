61b05f68f1f3310a1ebe84eab3e1aedafd66abd6
Tsimafei <tima.kutsko@gmail.com>
2023-05-01 16:13:00 +0300

    ClinicServiceV2. gRPC - переход на версию .net 7.0
    Универсальное приложение, которое может работать с gRPC и с RestAPI

---
+++ ClinicService.v2.0/ClinicService.Data/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ClinicService.Data
+{
+    [Table("Pets")]
+    public class Pet
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        public int ClientId { get; set; }
+
+        [Column, StringLength(50)]
+        public string Name { get; set; }
+
+        [Column]
+        public DateTime Birthday { get; set; }
+
+        public Client Client { get; set; }
+
+        [InverseProperty(nameof(Consultation.Pet))]
+        public ICollection<Consultation> Consultations { get; set; } = new HashSet<Consultation>();
+    }
+}
---
+++ ClinicService.v2.0/ClinicService/Services/Impl/ClinicService.cs
@@ -0,0 +1,89 @@
+﻿using ClinicService.Data;
+using ClinicService.Data.Context;
+using ClinicServiceNamespace;
+using Grpc.Core;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using static ClinicServiceNamespace.ClinicService;
+
+namespace ClinicService.Services.Impl
+{
+    public class ClinicService : ClinicServiceBase
+    {
+        private readonly ClinicServiceDbContext _context;
+
+        public ClinicService(ClinicServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        public override Task<CreateClientResponse> CreateClient(CreateClientRequest request, ServerCallContext context)
+        {
+            CreateClientResponse response;
+            try
+            {
+                Client client = new Client ()
+                {
+                    Document = request.Document,
+                    Surname = request.Surname,
+                    FirstName = request.FirstName,
+                    Patronymic = request.Patronymic,
+                };
+
+                _context.Clients.Add(client);
+                _context.SaveChanges();
+
+                response = new CreateClientResponse()
+                {
+                    ClientId = client.Id,
+                    ErrCode = 0,
+                    ErrMessage = string.Empty,
+                };
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+
+                response = new CreateClientResponse
+                {
+                    ErrCode = 1000,
+                    ErrMessage = $"Ошибка создания клиента: {ex.Message}",
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+
+        public override Task<GetClientsResponse> GetClients(GetClientsRequest request, ServerCallContext context)
+        {
+            GetClientsResponse response;
+            try
+            {
+                response = new GetClientsResponse();
+
+                response.Clients.AddRange(_context.Clients.Select(c => new ClientResponse
+                {
+                    ClientId = c.Id,
+                    Document = c.Document,
+                    Surname = c.Surname,
+                    FirstName = c.FirstName,
+                    Patronymic = c.Patronymic,
+                }));
+
+                return Task.FromResult(response);
+            }
+            catch (Exception ex)
+            {
+                response = new GetClientsResponse
+                {
+                    ErrCode = 1005,
+                    ErrMessage = $"Ошибка получения списка клиентов: {ex.Message}",
+                };
+
+                return Task.FromResult(response);
+            }
+        }
+    }
+}
