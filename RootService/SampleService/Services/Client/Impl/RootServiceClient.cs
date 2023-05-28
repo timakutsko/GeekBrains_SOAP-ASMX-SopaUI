73a8be71aa0ba24c1a3fd1a951adba9fc0f47a1e
Tsimafei <tima.kutsko@gmail.com>
2023-05-28 16:39:17 +0300

    SampleService. Polly, NLog

---
+++ RootService/SampleService/Services/Client/Impl/RootServiceClient.cs
@@ -0,0 +1,24 @@
+﻿using RootServiceNamespace;
+using System.Net.Http;
+
+namespace SampleService.Services.Client.Impl
+{
+    public class RootServiceClient : IRootServiceClient
+    {
+        private readonly ILogger<RootServiceClient> _logger;
+        private readonly RootServiceNamespace.RootServiceClient _rootServiceClient;
+
+        public RootServiceClient(ILogger<RootServiceClient> logger, HttpClient httpClient)
+        {
+            _logger = logger;
+            _rootServiceClient = new("http://localhost:5284", httpClient);
+        }
+
+        RootServiceNamespace.RootServiceClient IRootServiceClient.RootServiceClient => _rootServiceClient;
+
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return await _rootServiceClient.GetWeatherForecastAsync();
+        }
+    }
+}
